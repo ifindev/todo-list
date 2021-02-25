a0941b2b981ddc830b224f931150705cb24c3ba7
muhammad.arifin <arifin.muhammad2610@gmail.com>
2021-02-26 00:06:21 +0700

    Change delete button with unicode char

--- src/components/DeleteTodo.js
+++ src/components/DeleteTodo.js
@@ -1,7 +1,7 @@
 const DeleteTodo = ({ onClick, ...props }) => {
   return (
     <div {...props}>
-      <button onClick={onClick} className="btn delete-btn">&#10007;</button>
+      <button onClick={onClick} className="btn util-btn">&#10007;</button>
     </div>
   )
 }
